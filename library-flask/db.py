ef47bb663f1636225cd6fc92b8f6eeebaecfd6f0
adriel1ft <adriel.ferreira@academico.ufpb.br>
2025-03-04 21:37:22 -0300

    chore: coloquei dados do bd num .env

--- library-flask/db.py
+++ library-flask/db.py
@@ -1,14 +1,17 @@
 
 import mysql.connector
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
 #Conectando ao Banco de Dados
 def get_db():
     db = mysql.connector.connect(
-            host='localhost',#'0.tcp.sa.ngrok.io',
-            user='root',
-            password= '39*72p16lf',
-            database=  'biblioteca',
+            host= os.getenv('DB_HOST'),
+            user= os.getenv('DB_USER'),
+            password= os.getenv('DB_PASSWORD'),
+            database=  os.getenv('DB_NAME'),
             #port=18446,
             ssl_disabled=True)  
     
